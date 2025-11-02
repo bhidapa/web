a8d7dc3c58e1a0534b85f05a5470c586ba2a4956
Denis Badurina <denis@denelop.com>
2025-11-02 17:12:38 +0100

    prepare elibrary plugin

---
+++ wp-plugins/e-library/e-library.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * Plugin Name: E-Library
+ * Author:      BHIDAPA
+ * Author URI:  https://bhidapa.ba/
+ * Description: A simple e-library plugin for managing access to protected resources.
+ * Version:     0.0.0
+ */
