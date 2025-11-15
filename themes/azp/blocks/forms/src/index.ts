115085f9d7d71bd0b276c8d6635ae4b5d313ef3d
Denis Badurina <denis@denelop.com>
2025-11-15 19:21:52 +0100

    ready styles

---
+++ themes/azp/blocks/forms/src/index.ts
@@ -0,0 +1,4 @@
+// loaded on both editor and frontend. improt scss to compile it
+// wp-scripts will build and output a css file to build/style-index.css
+// this is why we need to `"style": "file:./style-index.css"` in block.json
+import './style.scss';
