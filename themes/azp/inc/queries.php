34969fec83e329f0faf11c2844d8648742ec0203
Denis Badurina <denis@denelop.com>
2026-05-12 14:29:12 +0200

    natural sort

--- themes/azp/inc/queries.php
+++ themes/azp/inc/queries.php
@@ -1,4 +1,14 @@
 <?php
+function natural_sort_orderby()
+{
+    global $wpdb;
+    // remove immediately so it doesn't bleed into other queries on the same request
+    remove_filter('posts_orderby', 'natural_sort_orderby');
+    // extract the first number from the title and sort by it as an integer,
+    // so "MODUL 2" comes before "MODUL 12" instead of after
+    return "CAST(REGEXP_SUBSTR({$wpdb->posts}.post_title, '[0-9]+') AS UNSIGNED) ASC, {$wpdb->posts}.post_title ASC";
+}
+
 add_action('pre_get_posts', 'custom_posts_order');
 function custom_posts_order(WP_Query $query)
 {
@@ -7,11 +17,9 @@ function custom_posts_order(WP_Query $query)
         $query->set('orderby', 'date');
     }
     if (is_tax('studijska-grupa')) {
-        $query->set('order', 'ASC');
-        $query->set('orderby', 'date');
+        add_filter('posts_orderby', 'natural_sort_orderby');
     }
     if (is_tax('edukacijski-program')) {
-        $query->set('order', 'ASC');
-        $query->set('orderby', 'date');
+        add_filter('posts_orderby', 'natural_sort_orderby');
     }
 }
